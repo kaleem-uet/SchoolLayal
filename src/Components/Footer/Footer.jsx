0e0c2431e43ce713395a32391157ba60262fdfc7
kaleem ullah <71625133+kaleem-uet@users.noreply.github.com>
2023-01-24 21:23:30 +0500

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,118 +4,129 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import { FooterLink, FooterTypo } from "../../Common/CustomTypo";
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import { styled } from "@mui/material";
 
 function Footer() {
   return (
     <Wrapper>
-      <Container
-        maxWidth="lg"
-        sx={{ alignItems: "center", justifyContent: "center", display: "flex" }}
-      >
+      <CustomContainer
+        maxWidth="lg"  >
         <Grid
           container
-          spacing={0}
+          spacing={4}
           sx={{ alignItems: "center", justifyContent: "center" }}
         >
-          <Grid items md={2.4}>
+          <Grid item md={2.4}  xs={12} sm={12}>
             <FooterTypo variant="h5">Company Info</FooterTypo>
-             <div style={{display:"flex",flexDirection:"column"}}>
-             <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+            <FooterColumn>
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              
-             </div>
-              
+            </FooterColumn>
           </Grid>
-          <Grid items md={2.4}>
+          <Grid item md={2.4}  xs={12} sm={12}>
             <FooterTypo variant="h5">Legal</FooterTypo>
-            <div style={{display:"flex",flexDirection:"column"}}>
-             <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+            <FooterColumn>
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              
-             </div>
-              
+            </FooterColumn>
           </Grid>{" "}
-          <Grid items md={2.4}>
+          <Grid item md={2.4}  xs={12} sm={12}>
             <FooterTypo variant="h5">Features</FooterTypo>
-            <div style={{display:"flex",flexDirection:"column"}}>
-             <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+            <FooterColumn>
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              
-             </div>
-              
+            </FooterColumn>
           </Grid>{" "}
-          <Grid items md={2.4}>
+          <Grid item md={2.4}  xs={12} sm={12}>
             <FooterTypo variant="h5">Resources</FooterTypo>
-            <div style={{display:"flex",flexDirection:"column"}}>
-             <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+            <FooterColumn>
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
+              <FooterLink variant="subtitle1" underLine="hover">
+                Contact us
               </FooterLink>
-              
-             </div>
-              
+            </FooterColumn>
           </Grid>{" "}
-          <Grid items md={2.4}>
+          <Grid item md={2.4}  xs={12} sm={12}>
             <FooterTypo variant="h5">Get In Touch</FooterTypo>
-            <div style={{display:"flex",flexDirection:"column"}}>
-             <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
-              </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
-              </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
-              </FooterLink>
-              <FooterLink variant="subtitle1" underLine="hover" >
-                  Contact us
-              </FooterLink>
-              
-             </div>
-              
+            <FooterColumn>
+              <IconAndTextContainer>
+                <PhoneOutlinedIcon sx={{ color: "#FFF", marginRight: 1 }} />
+                <FooterLink variant="subtitle1" underLine="hover">
+                  (480) 555-0103
+                </FooterLink>
+              </IconAndTextContainer>
+              <IconAndTextContainer>
+                <LocationOnOutlinedIcon
+                  sx={{ color: "#FFF", marginRight: 1 }}
+                />
+                <FooterLink variant="subtitle1" underLine="hover">
+                  4517 Washington Ave. Manchester, Kentucky 39495
+                </FooterLink>
+              </IconAndTextContainer>
+              <IconAndTextContainer>
+                <EmailOutlinedIcon sx={{ color: "#FFF", marginRight: 1 }} />
+                <FooterLink variant="subtitle1" underLine="hover">
+                  debra.holt@example.com
+                </FooterLink>
+              </IconAndTextContainer>
+            </FooterColumn>
           </Grid>
         </Grid>
-      </Container>
+      </CustomContainer>
     </Wrapper>
   );
 }
 
+const IconAndTextContainer=styled('div')(({theme})=>({
+  display: "flex",
+  alignItems: "flex-start",
+  flexDirection: "row",
+}))
+const FooterColumn=styled('div')(({theme})=>({
+  display: "flex", flexDirection: "column"
+}))
+const CustomContainer=styled(Container)(({theme})=>({
+  alignItems: "center", justifyContent: "center", display: "flex"
+}))
 export default Footer;
